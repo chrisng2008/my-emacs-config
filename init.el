5445df7d23b5841fe103a384e011e8d36351a7ef
Chris <chris2008@163.com>
2025-05-16 16:59:44 +0800

    修改字体配置

--- init.el
+++ init.el
@@ -15,7 +15,30 @@
 (tool-bar-mode -1)
 (setq cursor-type '(bar . 5))
 
-(global-set-key (kbd "C-h C-f") 'find-function)
+;;(global-set-key (kbd "C-h C-f") 'find-function)
 
 
+;; 同时设置英文字体和中文字体
+;;(set-face-attribute 'default nil :font "JetBrains Mono-16")
+;;(set-fontset-font t 'han (font-spec :family "PingFang SC"))
 
+(when (eq system-type 'darwin)
+  (setq fonts '("Jetbrains Mono" "PingFang SC"))
+  ;; 设置默认英文字体（JetbrainsMono NF Regular）
+  (set-face-attribute 'default nil :font
+                      (format "%s:pixelsize=%d" (car fonts) 16))
+  (set-fontset-font t 'han (font-spec :family (cadr fonts)))
+  ;; 添加emoji字体支持
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
+
+(when (eq system-type 'windows-nt)
+  (setq fonts '("JetbrainsMono NF Regular"))
+  (set-fontset-font t 'unicode "Segoe UI Emoji" nil 'prepend)
+  (set-face-attribute 'default nil :font
+                      (format "%s:pixelsize=%d" (car fonts) 24)))
+
+(when (eq system-type 'gnu/linux)
+  (setq fonts '("JetbrainsMono NF Regular"))
+  (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
+  (set-face-attribute 'default nil :font
+                      (format "%s:pixelsize=%d" (car fonts) 24)))
