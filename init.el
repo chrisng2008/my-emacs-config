7cd53262b5e7aa9b381db258975aee7131fc1c88
Chris <chris2008@163.com>
2025-05-17 19:46:53 +0800

    update config

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Emacs 换源
+(setq package-archives '(("gnu"    . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize) ;; You might already have this line
+
 (setq mac-option-modifier 'meta
       mac-command-modifier 'super)
 
@@ -68,10 +74,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company)))
+ '(custom-safe-themes
+   '("65a4ed5f62a21ad331b18db2bdd519e0404f9f34278051ce2a0807be8dc30d6b"
+     default))
+ '(package-selected-packages
+   '(company flucui-themes org-preview-html org-preview-html-mode))
+ '(warning-suppress-log-types '((ox-latex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;latex preview
+;;(require 'org-latex-preview)
+;;(setq org-latex-preview-auto-generate t) ; 自动预览
+
+;; html preview
+(require 'org-preview-html)
+
+;; org mode 换行
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+;;(flucui-themes-load-style 'light)
+;;(flucui-themes-load-style 'dark)
