d542ef176d1aedb5704a73a506d783a8af9c8588
Chris <chris2008@163.com>
2025-06-05 15:01:40 +0800

    update theme

--- init.el
+++ init.el
@@ -134,7 +134,7 @@
 ;; org mode 换行
 ;;(add-hook 'org-mode-hook 'visual-line-mode)
 
-(load-theme 'doom-dracula t)
+(load-theme 'doom-one-light t)
 
 
 ;; 配置跨平台输入法方案
@@ -159,6 +159,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+     default))
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
